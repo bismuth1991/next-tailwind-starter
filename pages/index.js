90882b5d80a60793244c4df7eb82f217fa1dc61a
bismuth1991 <pbao.tran.unsw@gmail.com>
2020-01-17 21:48:02 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div class="flex items-center justify-center min-h-screen">
+      <h1 class="text-4xl font-bold text-red-500">
+        Hello Next.js with Tailwindcss!
+      </h1>
+    </div>
+  );
+};
+
+export default Index;
